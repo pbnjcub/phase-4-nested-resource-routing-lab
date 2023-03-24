6fe7f7694f8d61c1bb3c4a5666f36ce68731f4c0
Paul Jung <pbnjcub@gmail.com>
2023-03-24 08:44:13 -0400

    lab done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,13 @@ class ItemsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    user = find_user
-    items = Item.all
+
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
 
